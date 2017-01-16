98b43f5e2bc2ad22bbf9468604001a78ee11ef69
preeti1205 <preeti.bhandari1205@gmail.com>
2017-01-15 19:14:48 -0500

    linked list

--- Linked_list/loop_detection_linked_list.cpp
+++ Linked_list/loop_detection_linked_list.cpp
@@ -12,4 +12,22 @@ struct ListNode{
 
 class Solution {
 public:
+  ListNode* isLoop(ListNode* l1) {
+    ListNode* fast = l1;
+    ListNode* slow = l1;
+
+    while(fast != slow) {
+      fast = fast->next->next;
+      slow = slow->next;
+    }
+
+    ListNode* starter = l1;
+
+    while(slow != starter) {
+      starter = starter->next;
+      slow = slow->next;
+    }
+
+    return starter;
+  }
 };
