3b340985e5d370b696eb93e18fd100c2f8577f35
preeti1205 <preeti.bhandari1205@gmail.com>
2017-01-17 16:44:22 -0500

    bst

---
+++ graphs & trees/isBST.cpp
@@ -0,0 +1,21 @@
+/*
+CTCI problem 4 - chapter 5
+*/
+
+#include <iomanip>
+#include <iostream>
+using namespace std;
+
+struct ListNode{
+  int val;
+  ListNode* left;
+  ListNode* right;
+  ListNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+  isBST(ListNode* root) {
+
+  }
+};
