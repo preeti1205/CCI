e65e96ecf982a836b1d0143f025b3fad4c025fa1
preeti1205 <preeti.bhandari1205@gmail.com>
2017-01-20 23:25:57 -0500

    new probs

--- graphs & trees/build_order.cpp
+++ graphs & trees/build_order.cpp
@@ -13,3 +13,20 @@ struct TreeNode{
   vector <TreeNode*> children;
   TreeNode(int x) : val(x), parent(NULL), children({}) {}
 };
+
+class Solution {
+public:
+  void build_graph(vector<char> projects, vector<pair<char, char> > depend) {
+    int n = depend.size();
+    unordered_map<char, vector<char> > graph;
+    unordered_map<char, int> visited_children;
+    for(int i = 0; i < n; i++) {
+        auto it = depend[i];
+        if (graph.find(it.first) != graph.end()){
+            graph[it.first].push_back(it.second);
+        }
+        else graph[it.first] = {it.second};
+        visited_children[it.second]++;
+    }
+  }
+};
